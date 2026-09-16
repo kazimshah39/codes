54c13fb9d008336ac5340075e4510a95e1f8ee49
kazimshah39 <47777072+kazimshah39@users.noreply.github.com>
2026-09-16 21:23:45 +0500

    Refactor Tawk.to and Lucky Wheel tracking scripts

--- php/WordPress/tawk.to-and-lucky-wheel-GTM.php
+++ php/WordPress/tawk.to-and-lucky-wheel-GTM.php
@@ -1,42 +1,59 @@
 <?php
-
 add_action('wp_footer', function () {
-?>
-    <script>
-        (function($) {
-            'use strict';
+    ?>
+        <script>
+            (function($) {
+                'use strict';
 
-            window.dataLayer = window.dataLayer || [];
-            window.Tawk_API = window.Tawk_API || {};
+                window.dataLayer = window.dataLayer || [];
+                window.Tawk_API = window.Tawk_API || {};
 
-            // Tawk.to Chat Started
-            window.Tawk_API.onChatStarted = function() {
-                window.dataLayer.push({
-                    event: 'tawk_to_chat_started'
+                // ----------------------------------------------------
+                // Tawk.to: Track Chat Started (First Visitor Message)
+                // ----------------------------------------------------
+                var isChatStarted = false;
+
+                // Neutralize proactive greetings/triggers from falsely firing chat started
+                window.Tawk_API.onChatStarted = function() {};
+
+                // Fire event only when visitor actually sends a message
+                window.Tawk_API.onChatMessageVisitor = function() {
+                    if (isChatStarted) return;
+                    isChatStarted = true;
+
+                    window.dataLayer.push({
+                        event: 'tawk_to_chat_started'
+                    });
+
+                    if (typeof window.fbq === 'function') {
+                        window.fbq('trackCustom', 'tawk_to_chat_started');
+                    }
+                };
+
+                // Reset state when chat concludes
+                window.Tawk_API.onChatEnded = function() {
+                    isChatStarted = false;
+                };
+
+                // ----------------------------------------------------
+                // Lucky Wheel
+                // ----------------------------------------------------
+                $(document).on('wof:play', function(event, data) {
+                    window.dataLayer.push({
+                        event: 'lucky_wheel_submit',
+                        wheel_id: data.wheel || null,
+                        is_winner: Boolean(data.winning),
+                        segment_id: data.segment || null,
+                        segment_text: data.segment_text || '',
+                        prize: data.segment_prize || ''
+                    });
+
+                    if (typeof window.fbq === 'function') {
+                        window.fbq('trackCustom', 'lucky_wheel_submit');
+                    }
                 });
 
-                if (typeof window.fbq === 'function') {
-                    window.fbq('trackCustom', 'tawk_to_chat_started');
-                }
-            };
-
-            // Lucky Wheel
-            $(document).on('wof:play', function(event, data) {
-                window.dataLayer.push({
-                    event: 'lucky_wheel_submit',
-                    wheel_id: data.wheel || null,
-                    is_winner: Boolean(data.winning),
-                    segment_id: data.segment || null,
-                    segment_text: data.segment_text || '',
-                    prize: data.segment_prize || ''
-                });
-
-                if (typeof window.fbq === 'function') {
-                    window.fbq('trackCustom', 'lucky_wheel_submit');
-                }
-            });
-
-        })(jQuery);
-    </script>
-<?php
-}, 100);
+            })(jQuery);
+        </script>
+    <?php
+    }, 100);
